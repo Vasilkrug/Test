0ad1f317d530001079fff1d8a714be24feed2b14
Araevskii <anton.raevsky@gmail.com>
2021-11-10 21:44:51 +0300

    Пример функции суммирования чисел.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('app')
+import {sum} from "./functions/sum";
+
+/** вот тут я буду вызывать твои функции, также тут ты сможешь тестировать */
+/**
+ *
+ * @param name | название функции - обязательно передавать его
+ * @param customFunction | функция для выполнения
+ */
+const make = (name = '', customFunction) => {
+    if (!name) {
+        return console.error('No function name provided')
+    }
+    try {
+        console.log(`Result of function ${name} = `, customFunction)
+    } catch (e) {
+        console.log(`Error function ${name}, error: `, e)
+    }
+}
+
+/** пример вызова функции суммы */
+make('Суммирование значений', sum(0, 99, 20, 20, 21))
---
+++ functions/sum.js
@@ -0,0 +1,1 @@
+export const sum = (...args) => args.reduce((a, b) => Number(a) + Number(b), 0)
